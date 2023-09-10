18defdf1760bfbba20be29c3812f3de74303d9d5
Maciek <mdyrdal@student.agh.edu.pl>
2023-09-10 22:47:35 +0200

    Add most of project, removed .venv

---
+++ app.py
@@ -0,0 +1,110 @@
+import asyncio
+import json
+import secrets
+
+import websockets
+
+from connect4 import PLAYER1, PLAYER2, Connect4
+
+
+JOIN = {}
+
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+
+async def replay(websocket, game):
+    for player, column, row in game.moves.copy():
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+
+
+async def play(websocket, game, player, connected):
+    async for message in websocket:
+        event = json.loads(message)
+        assert event["type"] == "play"
+        column = event["column"]
+
+        try:
+            row = game.play(player, column)
+        except RuntimeError as exc:
+            await error(websocket, str(exc))
+            continue
+
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        websockets.broadcast(connected, json.dumps(event))
+
+        if game.winner is not None:
+            event = {
+                "type": "win",
+                "player": game.winner,
+            }
+            websockets.broadcast(connected, json.dumps(event))
+
+
+async def start(websocket):
+    game = Connect4()
+    connected = {websocket}
+
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
+
+    try:
+        event = {
+            "type": "init",
+            "join": join_key,
+        }
+        await websocket.send(json.dumps(event))
+        await play(websocket, game, PLAYER1, connected)        
+    finally:
+        del JOIN[join_key]
+
+
+async def join(websocket, join_key):
+    try:
+        game, connected = JOIN[join_key]
+    except KeyError:
+        await error(websocket, "Game not found.")
+        return
+
+    connected.add(websocket)
+    try:
+        await replay(websocket, game)
+        await play(websocket, game, PLAYER2, connected)
+    finally:
+        connected.remove(websocket)
+
+
+async def handler(websocket):
+    message = await websocket.recv()
+    event = json.loads(message)
+    assert event["type"] == "init"
+
+    if "join" in event:
+        await join(websocket, event["join"])
+    else:
+        await start(websocket)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ main.js
@@ -0,0 +1,66 @@
+import { createBoard, playMove } from "./connect4.js";
+
+function sendMoves(board, websocket) {
+    board.addEventListener("click", ({ target }) => {
+        const column = target.dataset.column;
+        if (column === undefined) {
+            return;
+        }
+        const event = {
+            type: "play",
+            column: parseInt(column, 10),
+        };
+        websocket.send(JSON.stringify(event));
+      });
+}
+
+function showMessage(message) {
+    window.setTimeout(() => window.alert(message), 50);
+}
+  
+function receiveMoves(board, websocket) {
+    websocket.addEventListener("message", ({ data }) => {
+        const event = JSON.parse(data);
+        switch (event.type) {
+            case "init":
+                document.querySelector(".join").href = "?join=" + event.join;
+                break;
+            case "play":
+                playMove(board, event.player, event.column, event.row);
+                break;
+            case "win":
+                showMessage(`Player ${event.player} wins!`);
+                websocket.close(1000);
+                break;
+            case "error":
+                showMessage(event.message);
+                break;
+            default:
+                throw new Error(`Unsupported event type: ${event.type}.`);
+        }
+    });
+}
+
+function initGame(websocket) {
+    websocket.addEventListener("open", () => {
+        const params = new URLSearchParams(window.location.search);
+        let event = { type: "init" };
+        if (params.has("join")) {
+            event.join = params.get("join");
+        } else {
+
+        }
+
+        websocket.send(JSON.stringify(event));
+    })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const board = document.querySelector(".board");
+    createBoard(board);
+
+    const websocket = new WebSocket("ws://localhost:8001/");
+    initGame(websocket);
+    receiveMoves(board, websocket);
+    sendMoves(board, websocket);
+});
